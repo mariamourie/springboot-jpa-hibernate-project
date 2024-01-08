9e773b5bd2f30b0778e7320759693f3de2d85271
Maria Eduarda Cruz <mariaeduardacruzcv@gmail.com>
2024-01-08 15:37:32 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -103,6 +103,14 @@ public class Order implements Serializable {
 		return items;
 	}
 
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem item: items) {
+			sum += item.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
